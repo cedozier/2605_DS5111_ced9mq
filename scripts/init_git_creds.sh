ffd77128772f7734434b3192d45f8bdba52bbb57
cedozier <cedozier9274@gmail.com>
2026-05-23 22:55:00 +0000

    saving two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER=cedozier9274@gmail.com
+NAME=cedozier
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
