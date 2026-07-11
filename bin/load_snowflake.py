3f02849809e75318a246b3736457a5f629c921ee
cedozier <cedozier9274@gmail.com>
2026-07-11 18:10:50 +0000

    update requirements to include snowflake

--- bin/load_snowflake.py
+++ bin/load_snowflake.py
@@ -40,7 +40,7 @@ def main():
             account=os.getenv('SF_ACCOUNT'),
             warehouse=os.getenv('SF_WAREHOUSE'),
             database=os.getenv('SF_DATABASE'),
-            schema=os.getenv('SF_SCHEMA')
+            schema=os.getenv('SF_SCHEMA'),
             role=os.getenv('SF_ROLE')
         )
         cs = ctx.cursor()
