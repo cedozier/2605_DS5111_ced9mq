d33fda93293411f7b0851a4f6c06a2aec68c10f2
cedozier <cedozier9274@gmail.com>
2026-08-01 18:56:51 +0000

    change BOOK_NAME to BOOK_TITLE to match tests

--- models/fct_book_mentions.sql
+++ models/fct_book_mentions.sql
@@ -3,7 +3,7 @@
 
 SELECT
     VIDEO_ID,
-    f.value::STRING AS BOOK_NAME,
+    f.value::STRING AS BOOK_TITLE,
     INSERTED_AT AS PROCESSED_AT
 FROM {{ ref('stg_youtube_transcripts') }},
 LATERAL FLATTEN(input => BOOK_NAMES_ARRAY) f
